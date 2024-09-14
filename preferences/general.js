adbe8973acda064945958a8febea40a863548439
maniacx <macs18max@gmail.com>
2024-09-14 01:17:34 -0700

    bluetoothToggle: Add option to sort devices by history

--- preferences/general.js
+++ preferences/general.js
@@ -14,6 +14,7 @@ export const  General = GObject.registerClass({
         'enable_battery_level_text',
         'swap_icon_text',
         'swap_icon_text_row',
+        'sort_devices_by_history',
     ],
 }, class General extends Adw.PreferencesPage {
     constructor(settings) {
@@ -47,6 +48,12 @@ export const  General = GObject.registerClass({
         this._settings.bind(
             'swap-icon-text',
             this._swap_icon_text,
+            'active',
+            Gio.SettingsBindFlags.DEFAULT
+        );
+        this._settings.bind(
+            'sort-devices-by-history',
+            this._sort_devices_by_history,
             'active',
             Gio.SettingsBindFlags.DEFAULT
         );
